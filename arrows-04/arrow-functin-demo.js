762f3304df08508a233373a71e2a5ff023b0d8b4
Yohanes Demissie <Yohanesdemissie@gmail.com>
2017-12-07 17:41:01 -0800

    code wars 04

--- arrows-04/arrow-functin-demo.js
+++ arrows-04/arrow-functin-demo.js
@@ -23,4 +23,14 @@ let logger = (input) => console.log('user logged:', input)
 // you can even remove the perentheses with this new method
 let logger = input => console.log('user logged:', input)
 
+// for loop shortened too from jQuery to arrow function here below
+let arr = [1, 2, 3, 4]
+// replacing this jQuery for each function
+// arr.forEach(function(ele, idx, arr) {
+//     arr[idx] * 2
+// }) which is saying for each element within the index of the array, return it multiplied by 2
+arr.forEach((ele, idx, arr) => arr[idx] * 2)
+
+
+
 
